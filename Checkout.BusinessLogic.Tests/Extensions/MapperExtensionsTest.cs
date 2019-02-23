75d5f6ee29da165e4e1a6e680d66574cdfa5d770
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2019-02-23 21:26:17 +0000

    Added Extensions Tests

---
+++ Checkout.BusinessLogic.Tests/Extensions/MapperExtensionsTest.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using Xunit;
+
+namespace Checkout.BusinessLogic.Tests.Extensions
+{
+    using Checkout.Extensions;
+    using Checkout.Inventory;
+    using Checkout.Location;
+    using Checkout.Models;
+    using Moq;
+    using System.Collections.Generic;
+
+    public class MapperExtensionsTest : BaseTest
+    {
+        public MapperExtensionsTest()
+        {
+            ConfigureMapper();
+            MockContext();
+        }
+
+        [Fact]
+        public void ItMapsAnObject()
+        {
+            var item = context.Country.First();
+
+            var map = item.Map<CountryDto>();
+
+            Assert.Equal(item.Id, map.Id);
+            Assert.Equal(item.IsoCode, map.IsoCode);
+            Assert.Equal(item.IsDefault, map.IsDefault);
+            Assert.Equal(item.IsActive, map.IsActive);
+        }
+
+        [Fact]
+        public void ItMapsAList()
+        {
+            var items = context.Country.ToList();
+            var mapped = items.MapList<CountryDto>();
+
+            for (var i = 0; i < mapped.Count; i++)
+            {
+                Assert.Equal(mapped[i].Id, items[i].Id);
+                Assert.Equal(mapped[i].IsoCode, items[i].IsoCode);
+                Assert.Equal(mapped[i].IsDefault, items[i].IsDefault);
+                Assert.Equal(mapped[i].IsActive, items[i].IsActive);
+            }
+        }
+
+        [Fact]
+        public void ItMapsToPagedResultFirstPage()
+        {
+            var pager = new PagerDto(0, 1);
+
+            var items = GetPaged(pager);
+            var result = items.MapPaged<ProductDto>(pager);
+
+            Assert.Equal(result.Pager.PageIndex, (int)0);
+            Assert.Equal(result.Pager.PageSize, (int)1);
+            Assert.Equal(result.Pager.Total, (int)5);
+            Assert.Equal(result.Pager.PageNumber, (int)1);
+            Assert.True(result.Pager.IsFirstPage);
+            Assert.False(result.Pager.IsLastPage);
+
+        }
+
+        [Fact]
+        public void ItMapsToPagedResultLastPage()
+        {
+            var pager = new PagerDto(4, 1);
+
+            var items = GetPaged(pager);
+            var result = items.MapPaged<ProductDto>(pager);
+
+            Assert.Equal(result.Pager.PageIndex, (int)4);
+            Assert.Equal(result.Pager.PageSize, (int)1);
+            Assert.Equal(result.Pager.Total, (int)5);
+            Assert.Equal(result.Pager.PageNumber, (int)5);
+            Assert.False(result.Pager.IsFirstPage);
+            Assert.True(result.Pager.IsLastPage);
+        }
+
+
+        IList<ProductEntity> GetPaged(PagerDto pager)
+        {
+            var lst = new List<ProductEntity>() {
+                new Mock<ProductEntity>().Object,
+                new Mock<ProductEntity>().Object,
+                new Mock<ProductEntity>().Object,
+                new Mock<ProductEntity>().Object,
+                new Mock<ProductEntity>().Object
+            }.AsQueryable();
+
+            return lst.Paged<ProductEntity>(pager).ToList();
+        }
+    }
+}
---
+++ Checkout.BusinessLogic.Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+
+namespace Checkout.BusinessLogic.Tests.Extensions
+{
+    public class StringExtensionsTest : BaseTest
+    {
+        public StringExtensionsTest()
+        {
+        }
+
+        [Fact]
+        public void ItDisplaysCurrency()
+        {
+            decimal value = 1.56M;
+
+            var result = value.AsCurrency("en-Gb");
+            Assert.True(string.Compare("£1.56", result).Equals(0));
+
+            result = value.AsCurrency("de-DE");
+            Assert.True(string.Compare("1,56 €", result).Equals(0));
+        }
+    }
+}
