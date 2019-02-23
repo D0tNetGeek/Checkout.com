4fb0044f69e0b8360a48ae1b795ebcb5c8465220
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2019-02-23 01:42:44 +0000

    Added Country controller

---
+++ Checkout.Web/Controllers/Api/v1/CountriesController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Checkout.Web.Controllers.Api.v1
+{
+    using Location;
+
+    /// <summary>
+    /// Countries REST API.static Endpoints for country specific data.
+    /// </summary>
+    [ApiVersion("1.0")]
+    public class CountriesController : BaseApiController
+    {
+        private ICountryService countryService;
+
+        public CountriesController(ICountryService countryService)
+        {
+            this.countryService = countryService;
+        }
+
+        /// <summary>
+        /// Gets a collection of countries available for order process.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Country> GetEnumerable() => countryService.Get();
+
+        /// <summary>
+        /// Gets a country by a given countryId
+        /// </summary>
+        /// <param name="countryId">A given country Id to search for.</param>
+        /// <returns>An object of Country, when found.</returns>
+        [HttpGet("{countryId}")]
+        public async Task<Country> GetTask(short countryId) => await countryService.GetByIdAsync(countryId);
+    }
+}
